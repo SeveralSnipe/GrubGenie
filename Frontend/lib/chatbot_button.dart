83db62a1c26c350dd2976b856f0c21bff69f37f9
SeveralSnipe <s.vishwakumar@gmail.com>
2024-02-24 19:05:07 +0530

    Finished Chatbot

--- Frontend/lib/chatbot_button.dart
+++ Frontend/lib/chatbot_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:grub_genie/chatbot.dart';
+import 'package:page_transition/page_transition.dart';
 
 class ChatbotButton extends StatelessWidget {
   final Function onPressed;
@@ -10,7 +12,12 @@ class ChatbotButton extends StatelessWidget {
     return Align(
       alignment: const Alignment(0.9, 0.9),
       child: FloatingActionButton(
-        onPressed: () => onPressed(),
+        onPressed: () => Navigator.push(
+            context,
+            PageTransition(
+                child: const Chatbot(),
+                type: PageTransitionType.rightToLeft,
+                duration: const Duration(milliseconds: 700))),
         child: const Icon(Icons.chat),
       ),
     );
--- Frontend/lib/models.dart
+++ Frontend/lib/models.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:grub_genie/Api%20code/service/chatbot_service.dart';
+import 'package:grub_genie/message.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 import 'foodcard.dart';
 
@@ -22,6 +26,35 @@ class NearFoodProvider extends ChangeNotifier {
         cardList.add(result);
       }
     }
+    notifyListeners();
+  }
+}
+
+class ChatbotProvider extends ChangeNotifier {
+  List<Message> messages = [];
+
+  Future<void> sendMessage(String message, String userId) async {
+    messages.add(Message(
+        content: Text(message,
+            style: GoogleFonts.josefinSans(
+              color: Colors.black87,
+              fontSize: 16,
+            )),
+        sentByMe: true));
+    messages.add(Message(
+        content: LoadingAnimationWidget.twistingDots(
+            leftDotColor: Colors.cyan, rightDotColor: Colors.cyan, size: 20),
+        sentByMe: false));
+    notifyListeners();
+    var chatbotResponse = await ChatbotService().getChatbot(message, userId);
+    messages.removeLast();
+    messages.add(Message(
+        content: Text(chatbotResponse!.response,
+            style: GoogleFonts.josefinSans(
+              color: Colors.black87,
+              fontSize: 16,
+            )),
+        sentByMe: false));
     notifyListeners();
   }
 }
