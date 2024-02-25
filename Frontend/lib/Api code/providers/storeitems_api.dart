ff912e913901426c5b90f5565d1341cbb30f89df
d-man1212 <itsmedharshan.shankar@gmail.com>
2024-02-25 23:16:32 +0530

    Store items

--- Frontend/lib/Api code/providers/storeitems_api.dart
+++ Frontend/lib/Api code/providers/storeitems_api.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:grub_genie/Api code/models/storeitems.dart';
 
 class StoreItemsApi {
-  Future<List<StoreItems>?> getStoreItems(String storeId) async {
+  Future<StoreItems?> getStoreItems(String storeId) async {
     var uri = Uri.parse(
         'http://10.0.2.2:5000/CommerceNearMeRouter/StoreItems/$storeId');
     var client = http.Client();
@@ -16,13 +16,12 @@ class StoreItemsApi {
     print('Got response');
     if (response.statusCode == 200) {
       print('Got successful response');
-      List<dynamic> jsonList =
-          jsonDecode(const Utf8Decoder().convert(response.bodyBytes));
-
-      List<StoreItems> storeItems =
-          jsonList.map((item) => StoreItems.fromJson(item)).toList();
-
-      return storeItems;
+      print(
+          storeItemsFromJson(const Utf8Decoder().convert(response.bodyBytes)));
+      return storeItemsFromJson(
+          const Utf8Decoder().convert(response.bodyBytes));
+    } else {
+      print('Error: ${response.statusCode}');
     }
     return null;
   }
